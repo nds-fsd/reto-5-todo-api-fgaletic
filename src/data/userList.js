3a6fc30a75ff65038a55467cc459f202d3f34db3
filip.galetic@gmail.com <filip.galetic@gmail.com>
2023-11-13 12:59:36 +0100

    added users and categories router and data list

---
+++ src/data/userList.js
@@ -0,0 +1,22 @@
+const userList = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@example.com",
+    password: "password123"
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    password: "password456"
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    email: "bobjohnson@example.com",
+    password: "password789"
+  }
+];
+
+module.exports = userList;
